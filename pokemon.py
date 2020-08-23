cfdc996cb15482ceab8719def3e3b264915ffe1e
Programmer-Sidharth <sidharthbhardwaj0812@gmail.com>
2020-08-23 13:20:22 +0530

    changed the items and made image to be at every list item

--- pokemon.py
+++ pokemon.py
@@ -2,7 +2,7 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.lang import Builder
 from kivy.uix.image import AsyncImage
-from kivymd.uix.list import MDList, OneLineIconListItem, TwoLineListItem, IconLeftWidget, OneLineAvatarListItem, ImageLeftWidget
+from kivymd.uix.list import MDList, ILeftBody, OneLineIconListItem, TwoLineListItem, IconLeftWidget, OneLineAvatarListItem, ImageLeftWidget
 from kivymd.uix.button import MDRectangleFlatButton
 import requests
 import re
@@ -44,14 +44,29 @@ ScreenManager:
 		ScrollView:
 			MDList:
 				id: data
+				TwoLineListItem:
+					id: pokName
+					text: "Name"
+					secondary_text: ""
 
+				TwoLineListItem:
+					id: pokWeight
+					text: "Weight"
+					secondary_text: ""
 
+				TwoLineListItem:
+					id: pokHeight
+					text: "Height"
+					secondary_text: ""
 
+				TwoLineListItem:
+					id: pokType
+					text: "Type"
+					secondary_text: ""
 '''
 
-toolbar_text = """
-
-"""
+class AsyncImageLeftWidget(ILeftBody, AsyncImage):
+    pass
 
 class Home(Screen):
 	pass
@@ -65,48 +80,48 @@ sm.add_widget(Profile(name="profile"))
 
 
 class PokemonApp(MDApp):
-    def build(self):
-        screen = Screen()
-        self.theme_cls.theme_style = "Light"
-        self.hlpr_str = Builder.load_string(helper_text)
-        screen.add_widget(self.hlpr_str)
-        self.listpokemons()
-        self.profile_now = ''
-        return screen
-    def listpokemons(self):
-    	url = "https://pokeapi.co/api/v2/pokemon?limit=10"
-    	r = requests.get(url)
-    	for a in r.json()['results']:
-    		no = a['url'].split("/")[-2]
-    		btn = MDRectangleFlatButton(text=f"see {a['name']}", on_press=lambda x:self.profile_view_of(no))
-    		image = AsyncImage(source=f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{no}.png')
-    		# icon_ = IconLeftWidget(icon="language-python")
-    		listItem = OneLineAvatarListItem(text=a['name']) #, on_release=self.profile_view_of(a['name'])
-    		listItem.add_widget(image)
-    		listItem.add_widget(btn)
-    		self.hlpr_str.get_screen('home').ids.ls.add_widget(listItem)
-    def profile_view_of(self, no):
-    	if self.profile_now != no:
-    		self.profile_now == no
-	    	url = f"https://pokeapi.co/api/v2/pokemon/{no}"
-	    	print(url)
-	    	r = requests.get(url)
-	    	data = r.json()
-	    	best_image = AsyncImage(source=data['sprites']['other']['official-artwork']['front_default'], size_hint_x=1.8, pos_hint={"center_x":0.5, "center_y":0.7})
-	    	self.hlpr_str.get_screen('profile').ids.image.add_widget(best_image)
-	    	name = data['species']['name']
-	    	weight = data['weight']
-	    	height = data['height']
-	    	pokemon_type = ""
-	    	for a in data['types']:
-	    		pokemon_type += f"{a['type']['name']} "
-	    		factors = {"name":name, "weight":weight, "height":height, 'type':pokemon_type}
-	    	for f in factors:
-	    		item = TwoLineListItem(text=f, secondary_text=str(factors[f]))
-	    		self.hlpr_str.get_screen('profile').ids.data.add_widget(item)
+	def build(self):
+		screen = Screen()
+		self.theme_cls.theme_style = "Light"
+		self.hlpr_str = Builder.load_string(helper_text)
+		screen.add_widget(self.hlpr_str)
+		self.listpokemons()
+		return screen
+	def listpokemons(self):
+		url = "https://pokeapi.co/api/v2/pokemon?limit=100"
+		r = requests.get(url)
+		for a in r.json()['results']:
+			pokemon_no = a['url'].split("/")[-2]
+			# https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/100.png
+			image = AsyncImageLeftWidget(source=f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{pokemon_no}.png')
+			# image = AsyncImageLeftWidget(source=f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon_no}.png')
+			listItem = OneLineAvatarListItem(text=a['name'], on_release=lambda x:self.profile_view_of(a['name'])) #, on_release=self.profile_view_of(a['name'])
+			listItem.add_widget(image)
+			self.hlpr_str.get_screen('home').ids.ls.add_widget(listItem)
+	def profile_view_of(self, name):
+		url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+		r = requests.get(url)
+		data = r.json()
+		print(data['sprites']['other']['official-artwork']['front_default'])
+		best_image = AsyncImageLeftWidget(source=data['sprites']['other']['official-artwork']['front_default'], size_hint_x=1.8, pos_hint={"center_x":0.5, "center_y":0.7})
+		self.hlpr_str.get_screen('profile').ids.image.add_widget(best_image)
+		
+		name = data['species']['name']
+		weight = data['weight']
+		height = data['height']
+		pokemon_type = ""
+		for a in data['types']:
+			pokemon_type += f"{a['type']['name']} "
+		
+
+		self.hlpr_str.get_screen('profile').ids.pokName.secondary_text = name
+		self.hlpr_str.get_screen('profile').ids.pokWeight.secondary_text = str(weight)
+		self.hlpr_str.get_screen('profile').ids.pokHeight.secondary_text = str(height)
+		self.hlpr_str.get_screen('profile').ids.pokType.secondary_text = pokemon_type
 
 
-	    	self.hlpr_str.get_screen('profile').ids.title_of_page.title = name
-    	self.hlpr_str.get_screen('profile').manager.current = "profile"
+		self.hlpr_str.get_screen('profile').ids.title_of_page.title = name
+		self.hlpr_str.get_screen('profile').manager.current = "profile"
+
 
 PokemonApp().run()
